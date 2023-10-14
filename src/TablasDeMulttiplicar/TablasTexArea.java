e7988260dfbbe10222429c408fa64a9f07388cb8
Jose Armando <armandorodriguezseg19@gmail.com>
2023-10-14 16:21:29 -0600

    Se guardo la configuracion inicial del desenio de la interfaz

---
+++ src/TablasDeMulttiplicar/TablasTexArea.java
@@ -0,0 +1,12 @@
+package TablasDeMulttiplicar;
+
+import javax.swing.*;
+
+public class TablasTexArea {
+    private JLabel lblTablas;
+    private JTextArea txtArea;
+    private JTextField textField1;
+    private JButton btnEnviar;
+    private JButton btnLimpiar;
+    private JButton btnCerrar;
+}
